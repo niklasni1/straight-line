50de986b2f4ca14df273575fc45e046414f5a368
John Nowak <john@johnnowak.com>
2014-03-12 12:47:59 -0400

    Fixed interpreter.mli

--- interpreter.mli
+++ interpreter.mli
@@ -1,4 +1,19 @@
-val interpStm : stm -> Env -> Env 
-val interpExp : exp -> Env -> Int 
+type id = string
 
-val prog:(Lexing.lexbuf -> token) -> Lexing.lexbuf -> stm option
+type binop = Plus | Minus | Times | Div
+
+type stm =
+    CompoundStm of stm * stm
+  | AssignStm of id * exp
+  | PrintStm of exp list
+
+and exp =
+    IdExp of id
+  | NumExp of int
+  | OpExp of exp * binop * exp
+
+module Env : Map.S with type key = id
+
+val interpStm : stm -> int Env.t -> int Env.t
+
+val interpExp : exp -> int Env.t -> int
